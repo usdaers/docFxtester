549e5349fc5fc8e1ce1ac9aa5c0f23876900d85d
Dina Li <usdaers@gmail.com>
2018-03-15 16:27:55 -0400

    test upload
    excluded package for docfx

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// class definition: generating docFx documentation
+    /// </summary>
+    public class Class1
+    {           
+           
+        public Class1()
+        {
+
+        }
+
+        /// <summary>
+        /// this is a great method
+        /// </summary>
+        /// <returns>meaningful message</returns>
+        public string HelloWorld(string testMe)
+        {
+            var inMessage = testMe;
+            return inMessage;
+        }
+    }
+    /// <summary>
+    /// test Program
+    /// </summary>
+    /// <returns>not a whole lot</returns>
+    class Program
+    {
+        static void Main()
+        {
+            Class1 testClass = new Class1();
+            string outTest = testClass.HelloWorld("YAY");
+            Console.WriteLine(outTest);
+            Console.WriteLine("this is the end of the world");
+        }
+    }
+
+}
